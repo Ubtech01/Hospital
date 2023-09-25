a7f01d46e681844fd85ff543ef365b3f39d9bcfc
Mr. Ubong <ubongtech01@gmail.com>
2023-09-25 05:31:25 +0100

    Add project files.

---
+++ Hospital.Utilities/DbInitializer.cs
@@ -0,0 +1,22 @@
+﻿using Hospital.Repositories;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Utilities
+{
+    public class DbInitializer: IDbInitializer
+    {
+        private UserManager<IdentityUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        private ApplicationDbContext _context;
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
