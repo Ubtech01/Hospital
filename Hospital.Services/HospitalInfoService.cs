b258b9d16d6e910d43ba152eeea313393b6cffd5
Mr. Ubong <ubongtech01@gmail.com>
2023-10-06 12:08:49 +0100

    Added paging

---
+++ Hospital.Services/HospitalInfoService.cs
@@ -0,0 +1,26 @@
+﻿using hospital.Utilities;
+using Hospital.Repositories.interfaces;
+using Hospital.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Services
+{
+    public class HospitalInfoService : IHospitalInfo
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public HospitalInfoService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public PagedResult<HospitalInfoViewModel> GetAll(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
